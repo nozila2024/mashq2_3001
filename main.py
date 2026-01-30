135c294c221deb52a6bcb79d72196e46ad9c29e3
𝒩𝑜𝓏𝒾𝓁𝒶 <nozilarajabboyeva@gmail.com>
2026-01-30 15:29:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#2-misol
+fruits = ["apple", "banana", "cherry"]
+fruits.append("kiwi")
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+
+fruits.insert(1, "peach")
+print(fruits)
+
+fruits.sort()
+print(fruits)
